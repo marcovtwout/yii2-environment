be03b15c32bc6aef7edada2e4911c5fbbdfcc0e4
Javier Zapata <javier.zapata@piensa.io>
2019-12-11 14:12:18 +0100

    Add missing instance declaration

--- example-index/tests._bootstrap.php
+++ example-index/tests._bootstrap.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$env = \marcovtwout\YiiEnvironment\Environment('TEST');
+$env = new \marcovtwout\YiiEnvironment\Environment('TEST');
 
 defined('YII_DEBUG') or define('YII_DEBUG', $env->yiiDebug);
 defined('YII_ENV') or define('YII_ENV', $env->getYiiEnv());
